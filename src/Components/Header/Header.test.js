f3327d631dbc29f2ca812230d545f96f8aaf083e
Jamie Webb <jamie.webb@duedil.com>
2018-03-31 23:52:20 +0100

    update test descriptions

--- src/Components/Header/Header.test.js
+++ src/Components/Header/Header.test.js
@@ -3,13 +3,13 @@ import ReactDOM from 'react-dom';
 import Header from './Header';
 import renderer from 'react-test-renderer';
 
-it('renders without crashing', () => {
+it('Renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<Header />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
 
-it('renders correctly', () => {
+it('Renders correctly', () => {
     const tree = renderer
       .create(<Header />)
       .toJSON();
